96ff493338c1181404acb75421a059000b0e02eb
mvchn <movchan@gmail.com>
2022-01-09 21:39:12 +0200

    Close #53

--- src/Dog.php
+++ src/Dog.php
@@ -6,8 +6,32 @@ class Dog implements Serviceable
 {
     static int $cost = 1;
 
+    private int $position;
+
+    private $attach;
+
     public function getCost(): int
     {
         return self::$cost;
+    }
+
+    public function patrol(Herd $herd) : Dog
+    {
+        $this->attach = $herd;
+        $this->position = $herd->getPosition();
+
+        return $this;
+    }
+
+    public function move($position = 1) : Dog
+    {
+        $this->position += $position;
+        return $this;
+        //TODO: check attached herd position
+    }
+
+    public function getAttach()
+    {
+        return $this->attach;
     }
 }
